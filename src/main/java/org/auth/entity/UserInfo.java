77687c190ff46b7f6bcb2a5fcb80cb8aca4426ec
shd0w-exe <ankuryadav9393@gmail.com>
2025-03-13 00:57:13 +0530

    done

--- src/main/java/org/auth/entity/UserInfo.java
+++ src/main/java/org/auth/entity/UserInfo.java
@@ -27,7 +27,7 @@ public class UserInfo {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-            name = "user_roles",
+            name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
